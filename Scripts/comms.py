bdbec5b9ddb3c62a22d7a037fd6f2634ce90277c
Parker <Pagrant@wpi.edu>
2019-03-05 04:05:49 -0500

    send when receive

--- Scripts/comms.py
+++ Scripts/comms.py
@@ -28,49 +28,69 @@ v5 = 0
 s1 = "+"
 # ser.write(b'100,100,200,100,330;')
 i = 0
+stillgood = True
+breaktime = time.time()
 while(1):
-    #
-    # if(int(v1)>=0):
-    #     s1 = "+"
-    # else:
-    #     s1 = "-"
-    #
-    # s2 = str(v2).zfill(3)
-    # s3 = str(v3).zfill(3)
-    # s4 = str(v4).zfill(3)
-    # s5 = str(v5).zfill(3)
-    #
-    #
-    # v1 = str(abs(int(v1))).zfill(3)
-    # v2 = str(abs(int(v2))).zfill(3)
-    # v3 = str(abs(int(v3))).zfill(3)
-    # v4 = str(abs(int(v4))).zfill(3)
-    # v5 = str(abs(int(v5))).zfill(3)
-    #
-    # printme = s1+v1+"+"+v2+"+"+v3+"+"+v4+"+"+v5
-    # print(printme)
-    # # time.sleep(.5)
-    # # for n in printme.encode("ASCII","ignore"):
-    # ser.write(printme.encode("ASCII","ignore"))
-    # print("send and sleep")
-    time.sleep(2)
+    # #
+    # # if(int(v1)>=0):
+    # #     s1 = "+"
+    # # else:
+    # #     s1 = "-"
+    # #
+    # # s2 = str(v2).zfill(3)
+    # # s3 = str(v3).zfill(3)
+    # # s4 = str(v4).zfill(3)
+    # # s5 = str(v5).zfill(3)
+    # #
+    # #
+    # # v1 = str(abs(int(v1))).zfill(3)
+    # # v2 = str(abs(int(v2))).zfill(3)
+    # # v3 = str(abs(int(v3))).zfill(3)
+    # # v4 = str(abs(int(v4))).zfill(3)
+    # # v5 = str(abs(int(v5))).zfill(3)
+    # #
+    # # printme = s1+v1+"+"+v2+"+"+v3+"+"+v4+"+"+v5
+    # # print(printme)
+    # # # time.sleep(.5)
+    # # # for n in printme.encode("ASCII","ignore"):
+    # # ser.write(printme.encode("ASCII","ignore"))
+    # # print("send and sleep")
+    # time.sleep(2)
+    # # print(bytearray())
+    # # print(ser.read_all().strip(b'\r\n'))
+    # #######################################
+
+    printme = "-024-024+000+000+000"
+    ser.write(printme.encode("ASCII","ignore"))
+    time.sleep(.2)
+    print("negative")
+    print(ser.read_all().strip(b'\r\n'))
+    # time.sleep(2)
     # print(bytearray())
     # print(ser.read_all().strip(b'\r\n'))
     #######################################
 
-    printme = "-022-022+000+000+000"
-    ser.write(printme.encode("ASCII","ignore"))
-    print("send and sleep")
-    print(ser.read_all().strip(b'\r\n'))
-    time.sleep(2)
-    # print(bytearray())
-    # print(ser.read_all().strip(b'\r\n'))
-    #######################################
 
-    printme = "+020+020+000+000+000"
+    printme = "+024+024+000+000+000"
     ser.write(printme.encode("ASCII","ignore"))
-    print("send and sleep")
-    print(ser.read_all().strip(b'\r\n'))
+    time.sleep(.2)
+    print("positive")
+    xx = ser.read_all().strip(b'\r\n')
+    print(xx)
+
+    if (not xx == "+024+024+000+000+000".encode("ASCII")) and stillgood and (i>4):
+        stillgood = False
+        breaktime = time.time() - breaktime
+    i+=1
+
+    if not stillgood:
+        print("stopped working at "+ str(breaktime))
+    else:
+        print("still working at " + str(time.time() - breaktime)+"s at a rate of 0.2s")
+
+    # print(ser.read_all().strip(b'\r\n'))
+
+
     # v5 = -5
     # if(i>10):
     #     v5 = 1
---
+++ Scripts/sendwhhenrecieve.py
@@ -0,0 +1,33 @@
+import time
+
+import serial
+ser = serial.Serial()
+ser.baudrate = 9600
+ser.port = 'COM14'
+ser.open()
+i = 0
+stillgood = True
+breaktime = time.time()
+while(1):
+    last = time.time()
+    # time.sleep(.1)
+    printme = "+024+024+000+000+000"
+    ser.write(printme.encode("ASCII","ignore"))
+    # time.sleep(.1)
+    print("positive")
+    xx = ser.read(20).strip(b'\r\n')
+    print(xx)
+    print(time.time()-last)
+    #
+    # if (not xx == "+024+024+000+000+000".encode("ASCII")) and stillgood and (i>4):
+    #     stillgood = False
+    #     breaktime = time.time() - breaktime
+    # i+=1
+    #
+    # if not stillgood:
+    #     print("stopped working at "+ str(breaktime))
+    # else:
+    #     print("still working at " + str(time.time() - breaktime)+"s at a rate of 0.2s")
+
+    print("still working at " + str(time.time() - breaktime)+"s at a rate of titties")
+
